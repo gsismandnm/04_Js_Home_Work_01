0a2c1b44cd0a47ceb6d055304278f68c8f4f2221
Gökan ŞİŞMAN <gknsismandenmark@gmail.com>
2021-02-18 02:36:46 +0200

    Update callback.js

--- callback.js
+++ callback.js
@@ -5,24 +5,27 @@
 //     3.BİLGİLER KAYDEDİLMESİ 
 //     4.KAYIT İŞLEMİNİN TAMAMLANMASI 
 
+
+//1. YENİ KAYIT EKRANININ AÇILMASI
 kayıtEkranı(bilgilerGirildi);
 
-console.log('Yeni Kayıt Ekranı Açılıyor. Lütfen Bekleyiniz.');
-
 function kayıtEkranı(bilgilerGirildi){
+    console.log('Yeni Kayıt Ekranı Açılıyor. Lütfen Bekleyiniz.');
     setTimeout(() => {
         console.log('Yeni Kayıt Ekranı Açıldı.');
         bilgilerGirildi(sistemeKaydedildi);
     }, 3500);
 }
 
+//2.KİMLİK BİLGİLERİNİN GİRİLMESİ
 function bilgilerGirildi(sistemeKaydedildi) {
     setTimeout(() => {
-        console.log('Kimlik Bilgileri Girildi');
+        console.log('Kimlik Bilgileri Giriliyor');
         sistemeKaydedildi(kayıtTamamlandı)
     }, 2000);
 }
 
+//3.BİLGİLERİN SİSTEME KAYDEDİLMESİ
 function sistemeKaydedildi(kayıtTamamlandı) {
     setTimeout(() => {
         console.log('Bilgiler Kaydediliyor Lütfen Bekleyin...');
@@ -30,6 +33,7 @@ function sistemeKaydedildi(kayıtTamamlandı) {
     }, 1000);
 }
 
+//4.KAYIT İŞLEMİNİN TAMAMLANMASI
 function kayıtTamamlandı(){
     setTimeout(() => {
         console.log('Kayıt İşlemi Başarı İle Tamamlandı.');        
